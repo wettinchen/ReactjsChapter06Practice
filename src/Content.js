71b35e91aa67d30c9f826850c974dc5d3a24701e
suncrownfisher <suncrownfisher@gmail.com>
2023-03-16 22:39:38 +0800

    first practice

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,27 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       }
+
+    const handleClick = () => {
+        console.log('You clicked it');
+    };
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    };
+
+    const handleClick3 = (event) => {
+        console.log(event.target.innerText);
+    };
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => handleClick2('Dave')}>Click It</button>
+            <button onClick={(event) => handleClick3(event)}>Click It</button>
         </main>
     )
 }
